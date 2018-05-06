f6c91a95d5a10240e5350ecd1897e29b4accfc82
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2018-05-06 20:45:18 +0200

    update: delete and change view item

--- src/actions.js
+++ src/actions.js
@@ -5,3 +5,37 @@ export const addNewDictionary = (name: string) => {
     name: name
   };
 };
+
+export const addDomRag = (obj: object) => {
+  //console.log("actions obj", obj);
+  return {
+    type: "ADD_NEW_DOMAIN_RANGE",
+    domain: obj.domain,
+    range: obj.range,
+    dictionary: obj.dictionary
+  };
+};
+
+export const changeViewItem = viewItem => {
+  console.log("action change item", viewItem);
+  return {
+    type: "CHANGE_VIEW_ITEM",
+    viewItem: viewItem
+  };
+};
+
+export const deleteRow = obj => {
+  console.log("the actions for deleting a row", obj);
+  return {
+    type: "DELETE_ROW",
+    content: obj
+  };
+};
+
+export const EditRow = obj => {
+  console.log(obj);
+  return {
+    type: "EDIT_ROW",
+    content: obj
+  };
+};
