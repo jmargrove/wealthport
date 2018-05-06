4f37c0f169488b58df6df03aecfd01309258dc42
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2018-05-06 20:50:55 +0200

    feat: table design - not using react-tables anymore

--- src/presentational/Tables.js
+++ src/presentational/Tables.js
@@ -5,28 +5,29 @@ import TableComponent from "./../components/TableComponent.js";
 const ViewContentContainer = styled.div`
   width: 100%;
   height: 750px;
-  background-color: LightCoral;
+  background-color: red;
 `;
 
 const AddDictionaryContainer = styled.div`
   height: 60px;
-  width: 480px;
+  width: 500px;
   margin: 20px;
   margin-bottom: 0;
-  background-color: purple;
+  background-color: blue;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: grab;
   &:hover {
-    background-color: turquoise;
+    background-color: white;
   }
 `;
 
 const TableContainer = styled.div`
+  display: flex;
   min-height: 600px;
-  width: 480px;
-  background-color: white;
+  width: 500px;
+  background-color: orange;
   margin: 20px;
 `;
 
