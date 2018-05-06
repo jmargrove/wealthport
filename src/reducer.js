d92da66ca688cbdc163879c73256b8948c306aec
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2018-05-06 20:46:34 +0200

    working on the reducers, delete and edit

--- src/reducer.js
+++ src/reducer.js
@@ -1,24 +1,21 @@
 const defaultState = {
-  dictionaries: [
-    {
-      colors: {
-        domain: ["Stonegrey", "Midnight Black", "Mystic Silver"],
-        range: ["Dark grey", "Black", "Silver"]
-      }
-    },
-    {
-      size: {
-        domain: ["Massive", "Middle Sized", "Tiny"],
-        range: ["Large", "Medium", "Small"]
-      }
-    },
-    {
-      product: {
-        domain: ["Massive", "Middle Sized", "Tiny"],
-        range: ["Large", "Medium", "Small"]
-      }
-    }
-  ]
+  viewItem: "colors",
+  dictionaries: {
+    colors: [
+      { domain: "Stonegrey", range: "Dark grey" },
+      { domain: "Midnight Black", range: "Black" },
+      { domain: "Mystic Silver", range: "Silver" }
+    ],
+    size: [
+      { domain: "Massive", range: "Large" },
+      { domain: "Middle Sized", range: "Medium" }
+    ],
+    product: [
+      { domain: "Massive", range: "Large" },
+      { domain: "Middle Sized", range: "Medium" },
+      { domain: "Tiny", range: "Small" }
+    ]
+  }
 };
 
 const reducer = (state = defaultState, action) => {
@@ -26,15 +23,48 @@ const reducer = (state = defaultState, action) => {
     case "ADD_NEW_DICTIONARY_NAME": {
       return {
         ...state,
-        dictionaries: [
+        dictionaries: {
           ...state.dictionaries,
-          {
-            [action.name]: {
-              domain: [],
-              range: []
+          [action.name]: [
+            {
+              domain: "",
+              range: ""
             }
-          }
-        ]
+          ]
+        }
+      };
+    }
+    case "ADD_NEW_DOMAIN_RANGE": {
+      return {
+        ...state,
+        dictionaries: {
+          ...state.dictionaries,
+          [action.dictionary]: [
+            ...state.dictionaries[action.dictionary],
+            { domain: action.domain, range: action.range }
+          ]
+        }
+      };
+    }
+    case "CHANGE_VIEW_ITEM": {
+      console.log("action.......dksl", action.viewItem);
+
+      return {
+        ...state,
+        viewItem: action.viewItem
+      };
+    }
+    case "DELETE_ROW": {
+      return {
+        ...state,
+        dictionaries: {
+          ...state.dictionaries,
+          [action.content.dictionary]: [
+            ...state.dictionaries[action.content.dictionary].filter(
+              (el, i) => i !== action.content.rowNumber
+            )
+          ]
+        }
       };
     }
     default:
