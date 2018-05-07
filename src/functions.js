2c31f5695b48355ba253dea16c306d69e0a25738
t03jam8 <j.margrove.08@aberdeen.ac.uk>
2018-05-07 14:32:37 +0200

    feat: functions for the testing, 2 recursive NOT HAPPY

---
+++ src/functions.js
@@ -0,0 +1,95 @@
+exports.testingDuplicateRows = array => {
+  let j = 1;
+  function recur() {
+    for (let i = 0; i < array.length; i++) {
+      if (j > array.length - 1) {
+        return array;
+      }
+
+      if (
+        array[i].domain === array[j].domain &&
+        array[i].range === array[j].range
+      ) {
+        console.log("i = ", i, "j = ", j);
+        if (i === j) {
+          return array;
+        } else {
+          array[i].testResult = "duplicate row";
+          array[j].testResult = "duplicate row";
+          return array;
+        }
+      }
+      j++;
+      recur();
+      j--;
+    }
+    return array;
+  }
+  return recur();
+};
+
+exports.testingDuplicateDomains = array => {
+  let j = 1;
+  function recur() {
+    for (let i = 0; i < array.length; i++) {
+      if (j > array.length - 1) {
+        return array;
+      }
+
+      if (array[i].domain === array[j].domain) {
+        console.log("i = ", i, "j = ", j);
+        if (i === j) {
+          return array;
+        } else {
+          array[i].testResult = "duplicate domain";
+          array[j].testResult = "duplicate domain";
+          return array;
+        }
+      }
+      j++;
+      recur();
+      j--;
+    }
+    return array;
+  }
+  return recur();
+};
+
+exports.testingCycles = array => {
+  // reset values
+  array = array.map(el => {
+    el.testResult = "";
+    return el;
+  });
+  // find the cycle ones
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (
+        array[i].domain === array[j].range &&
+        array[j].domain === array[i].range
+      ) {
+        array[i].testResult = "Cycle";
+        array[j].testResult = "Cycle";
+      }
+    }
+  }
+  return array;
+};
+
+exports.testingChain = array => {
+  // reset values
+  array = array.map(el => {
+    el.testResult = "";
+    return el;
+  });
+  // find the chains
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[i].domain === array[j].range) {
+        array[i].testResult = "Chain";
+        array[j].testResult = "Chain";
+      }
+    }
+  }
+  return array;
+};
